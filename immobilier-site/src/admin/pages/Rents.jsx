95324aa2f23531b45fb6fef95afdeb80f5dc9ae1
abdousar2 <abdou_sar2@yahoo.fr>
2026-08-02 23:31:54 +0000

    direct update MAREGA

--- immobilier-site/src/admin/pages/Rents.jsx
+++ immobilier-site/src/admin/pages/Rents.jsx
@@ -28,8 +28,6 @@ export default function Rents() {
 
     const location = useLocation();
 
-    const location = useLocation();
-
     useEffect(() => {
 
         const params = new URLSearchParams(location.search);
